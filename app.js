532e641e1bd3e3821280a538ebfbea10518e501f
Huseyn Iskandarli <huseyn.iskandarli@gmail.com>
2019-03-17 16:14:03 +0000

    Added Mongoose and Mongod binding

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+var mongoose = require("mongoose");
 var express = require("express");
 var app = express();
 
+mongoose.connect("mongodb://localhost:27017/uni-and-me-1", {useNewUrlParser: true});
+
 app.get("/", function(req,res){
     res.send("/ page");
+    
 }); 
 
 app.listen(3000, function(){

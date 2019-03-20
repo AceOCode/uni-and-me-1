31930665a786781eb728af421fb4bbf7aedd6883
AceOCode <h.iskandarli@lse.ac.uk>
2019-03-20 13:08:00 +0000

    This updat adds the feature of adding new unis and corses while also referencing them to each other in the DB

--- app.js
+++ app.js
@@ -19,38 +19,77 @@ app.get("/", function(req,res){
 //INDEX - UNI
 app.get("/universities", function(req,res){
     University.find({}, function(err, allUniversities){
-        if(err){console.log(err)
-        }else{res.render("university/universities", {Universities: allUniversities})}
+        if(err){console.log(err);
+        }else{res.render("university/universities", {unis: allUniversities})}
     });
 });
 
 
 //CREATE - UNI
 app.post("/universities", function(req,res){
-    var name = req.body.name
-    var country = req.body.country
-    console.log(country);
-    //We need a function to add these to the DB
-    res.redirect("/universities");
+    University.create(req.body.uni, function(err, newlyCreated){
+        if(err){console.log(err);
+        }else{res.redirect("/universities")} 
+    });    
 });
 
 //NEW - UNI
 app.get("/universities/new", function(req,res){
-    res.render("university/new_uni");
+    res.render("university/new_uni"); 
 });
 
 //SHOW - UNI
 app.get("/universities/:id", function(req,res){
     University.findById(req.params.id, function(err,foundUni){
         if(err){console.log(err);
-        }else{res.render("university/show_uni", {university: foundUni})}    
+        }else{res.render("university/show_uni", {uni: foundUni})}    
     }); 
 });
 
+//==================================================================
+// COURSE COURSE COURSE COURSE COURSE COURSE COURSE COURSE COURSE
+//==================================================================
+
 //INDEX - COURSE
+//We want to view all of our courses in this route.
 app.get("/courses", function(req,res){
-    res.render("courses");
+    res.send("This is the INDEX - COURSE route")
 });
+
+//SHOW - COURSE
+//We want to be able to have detailed information about each individual course.
+app.get("/universities/:id/courses/:course_id", function(req,res){
+    // Add a find course by id and pass into the ejs file in the res.render as "course".
+    res.render("course/show_course");
+});
+
+//CREATE - COURSE
+//This updates the DB to have new courses for the respective university.
+app.post("/courses", function(req,res){
+    University.findOne({name: req.body.course.university}, function(err,foundUniversity){
+        if(err){console.log(err);
+        }else{
+            var course = {name: req.body.course.name, level: req.body.course.level, university: foundUniversity}
+            Course.create(course, function(err, newCourse){
+                if(err){console.log(err);
+                }else{
+                    foundUniversity.courses.push(newCourse);
+                    foundUniversity.save();
+                    res.redirect("/universities/" + foundUniversity._id);
+                }
+            });
+        }
+    });
+});
+//NEW -COURSE
+app.get("/courses/new", function(req,res){
+    University.find({}, function(err,universities){
+        if(err){console.log(errr);
+        }else{res.render("course/new_course", {universities: universities})}
+    });
+});
+
+//SHOW - COURSE
 
 app.listen(3000, function(){
     console.log("Server Online");
